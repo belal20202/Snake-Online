47a8c1b8ef9778618b84baa1176c1c230497ec89
belal20202 <belalaliraqi9@gmail.com>
2026-09-04 23:34:05 +0300

    Update global.gd

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,6 +1,15 @@
 extends Node
 
-var player_name: String = ""
+var player_name: String = "لاعب"
+
 var coins: int = 0
 var experience: int = 0
 var level: int = 1
+
+var last_score: int = 0
+var last_length: int = 10
+
+
+func reset_match_stats() -> void:
+	last_score = 0
+	last_length = 10
