afa5a3d38ee25c6b3fafb0fddb0572c3d3bcbacd
belal20202 <belalaliraqi9@gmail.com>
2026-09-04 23:34:51 +0300

    Update main_menu.gd

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,15 @@
 extends Control
 
+
 func _ready() -> void:
-    $CenterContainer/VBoxContainer/PlayButton.grab_focus()
+	$CenterContainer/VBoxContainer/PlayButton.grab_focus()
+
 
 func _on_play_button_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/player_name.tscn")
+	get_tree().change_scene_to_file(
+		"res://scenes/player_name.tscn"
+	)
+
 
 func _on_exit_button_pressed() -> void:
-    get_tree().quit()
+	get_tree().quit()
